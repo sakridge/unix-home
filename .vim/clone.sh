cd7e6efc0e9fb92cded5cbd322f351439cb1645a
Stephen Akridge <sakridge@gmail.com>
2018-07-15 10:52:46 +0000

    Remove autoload/plugin stuff and update clone script

--- .vim/clone.sh
+++ .vim/clone.sh
@@ -1,4 +1,16 @@
 mkdir -p bundle
-git clone https://github.com/tpope/vim-pathogen.git bundle/vim-pathogen
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrp.vim
-git clone https://github.com/rust-lang/rust.vim.git bundle/rust.vim
+
+clone_or_update () {
+    if [ -d "$2" ]; then
+        echo "Pulling.. $2"
+        pushd $2 && git pull
+        popd
+    else
+        echo "Cloning.. $2"
+        git clone $1 $2
+    fi
+}
+clone_or_update https://github.com/tpope/vim-pathogen.git bundle/vim-pathogen
+clone_or_update https://github.com/kien/ctrlp.vim.git bundle/ctrp.vim
+clone_or_update https://github.com/rust-lang/rust.vim.git bundle/rust.vim
+clone_or_update https://github.com/ldx/vim-indentfinder.git bundle/vim-indentfinder
