d26a409b7fc2165f60cc4ca7b9f8f3c1091f474a
Stephen Akridge <sakridge@gmail.com>
2020-04-24 11:57:39 -0700

    add create gce script

--- bin/create-gce.sh
+++ bin/create-gce.sh
@@ -2,4 +2,5 @@ if [ "$#" -ne "1" ]; then
     echo "wrong arguments: Usage $0 <num-nodes>"
     exit 1
 fi
-./net/gce.sh create -n $1 -c 1 -d pd-ssd --custom-machine-type "--custom-cpu 16 --custom-memory 32GB --min-cpu-platform Intel%20Skylake" --dedicated
+#./net/gce.sh create -n $1 -c 1 -d pd-ssd --custom-machine-type "--custom-cpu 16 --custom-memory 32GB --min-cpu-platform Intel%20Skylake" --dedicated
+./net/gce.sh create -n $1 -c 1 -d pd-ssd -G "--custom-cpu 16 --custom-memory 32GB --min-cpu-platform Intel%20Skylake --accelerator count=2,type=nvidia-tesla-v100" --dedicated
