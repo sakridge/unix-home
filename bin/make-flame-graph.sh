b86fdac0dc02a289c18a080483384089fdc6be69
Stephen Akridge <sakridge@gmail.com>
2022-01-15 07:40:27 -0800

    Add comments on how to get flamegraph dependencies

--- bin/make-flame-graph.sh
+++ bin/make-flame-graph.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#cppfilt install:
+# cargo install cpp_demangle --example cppfilt
+# from https://github.com/gimli-rs/cpp_demangle
+
+# src/FlameGraph from github: https://github.com/brendangregg/FlameGraph
+
+# rust-unmangle from github:
+# https://github.com/Yamakaky/rust-unmangle
+
 sudo perf script > perf-script.out
 cat perf-script.out | ~/src/FlameGraph/stackcollapse-perf.pl > perf-folded.out
 cat perf-folded.out | cppfilt > perf-cppfilt.out
